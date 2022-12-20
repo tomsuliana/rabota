6a57ad6d775a315c54281f3cef26b4c5291d4818
tomsuliana <ultomskaya@yandex.ru>
2022-12-20 23:02:51 +0300

    Some localization done

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :variants
-  resources :questions
-  get 'admin/panel'
-  get 'admin/control_users'
-  get 'admin/control_questions'
-  get 'exam/cabinet'
-  get 'exam/exam_process'
-  get 'exam/exam_result'
-  root "session#login"
-  post 'session/authorize'
-  get 'session/logout'
-  resources :users
-  post 'exam/exam_update'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    resources :variants
+    resources :questions
+    get 'admin/panel'
+    get 'admin/control_users'
+    get 'admin/control_questions'
+    get 'exam/cabinet'
+    get 'exam/exam_process'
+    get 'exam/exam_result'
+    root "session#login"
+    post 'session/authorize'
+    get 'session/logout'
+    resources :users
+    post 'exam/exam_update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  end
 end
