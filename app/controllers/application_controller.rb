3fcd96205a4c32929b0535c67a9346cafe053d50
tomsuliana <ultomskaya@yandex.ru>
2022-12-22 16:11:35 +0300

    Localization finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  around_action :switch_locale
 
   private
 
@@ -16,5 +17,18 @@ class ApplicationController < ActionController::Base
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ?
       parsed_locale.to_sym :
       nil
+  end
+
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url 
+    locale = params[:locale]
+
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+
+    nil
   end
 end
